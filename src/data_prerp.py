7030ba383d1bbf8a3bc147e9f9883f1e6e831e9c
Felipe Checa <felipe.checa@pm.me>
2025-05-29 17:17:58 +0100

    Added Third DataFrame

--- src/data_prerp.py
+++ src/data_prerp.py
@@ -3,3 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A": [1, 2, 3, 4, 5], "B": [5, 4, 3, 2, 1]})
+
+df2 = pd.DataFrame({"C": [6, 7, 8, 9, 10], "D": [10, 9, 8, 7, 6]})
+
+df3 = pd.DataFrame({"E": [11, 12, 13, 14, 15], "F": [15, 14, 13, 12, 11]})
